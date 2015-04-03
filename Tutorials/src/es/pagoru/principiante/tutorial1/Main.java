c92557817abb898b95d456d1926df59d3ee04172
pagoru <pagoru@gmail.com>
2015-04-03 20:20:21 +0200

    Tutorial 1

---
+++ Tutorials/src/es/pagoru/principiante/tutorial1/Main.java
@@ -0,0 +1,16 @@
+package es.pagoru.principiante.tutorial1;
+
+public class Main {
+		
+	public static void main(String[] args) {
+		
+		String holaMundo = "Hola mundo";
+		String adiosMundo = "Adios mundo";
+		
+		String todoMundo = holaMundo + " " + adiosMundo;
+		
+		System.out.println(todoMundo);
+
+	}
+
+}
